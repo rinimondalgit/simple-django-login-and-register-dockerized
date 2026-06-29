10a3eae844e23cf783065fbc1833ba22d1a640a3
rinimondalgit <rini.mondal@gmail.com>
2026-06-28 23:07:42 -0500

    Fix intentionally failing test

--- source/accounts/tests.py
+++ source/accounts/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 class AccountPageTests(TestCase):
     def test_login_page_loads(self):
-        self.fail("Intentional CI failure demo")
+        self.fail(response.status_code, 200)
 
     def test_signup_page_loads(self):
         response = self.client.get("/accounts/sign-up/")
