3c939d04d71bcd95751ad9030bc36582ca50cb6b
rinimondalgit <rini.mondal@gmail.com>
2026-06-28 23:15:23 -0500

    Fix account login page test

--- source/accounts/tests.py
+++ source/accounts/tests.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 
 class AccountPageTests(TestCase):
     def test_login_page_loads(self):
-        self.fail(response.status_code, 200)
+        response = self.client.get("/accounts/log-in/")
+        self.assertEqual(response.status_code, 200)
 
     def test_signup_page_loads(self):
         response = self.client.get("/accounts/sign-up/")
