dfcde2dd0032951f2f5fabf7ef421bd58ebcbe04
rinimondalgit <rini.mondal@gmail.com>
2026-06-28 22:32:13 -0500

    Force intentional CI failure

--- source/accounts/tests.py
+++ source/accounts/tests.py
@@ -3,13 +3,12 @@ from django.test import TestCase
 
 class AccountPageTests(TestCase):
     def test_login_page_loads(self):
-        response = self.client.get("/accounts/log-in/")
-        self.assertEqual(response.status_code, 500)
+        self.fail("Intentional CI failure demo")
 
     def test_signup_page_loads(self):
         response = self.client.get("/accounts/sign-up/")
         self.assertEqual(response.status_code, 200)
 
-    def test_restore_password_page_loads(self):
-        response = self.client.get("/accounts/restore/password/")
+    def test_register_page_loads(self):
+        response = self.client.get("/accounts/sign-up/")
         self.assertEqual(response.status_code, 200)
